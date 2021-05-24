daecd59795f6201abe4bd025f682c3aae0818e7f
Sergei <nickincorname@gmail.com>
2021-05-24 08:57:27 +0300

    Added CRD procedures for Feedbacks

---
+++ ClientsAgregator.Database/dbo/Stored Procedures/AddFeedbacks.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE PROCEDURE AddFeedbacks
+@Clientid INT,
+@Productid INT,
+@Description VARCHAR(800),
+@Date VARCHAR(255),
+@Rate INT
+
+AS
+INSERT Clients VALUES
+(@Clientid, @Productid, @Description, @Date, @Rate)
