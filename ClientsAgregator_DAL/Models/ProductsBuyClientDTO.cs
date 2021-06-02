97e426c3abb724de6f96fc26548a8d5d3a1f4a64
RDesti <tatotschka17@rambler.ru>
2021-06-03 00:34:22 +0300

    Playing with Wpf

---
+++ ClientsAgregator_DAL/Models/ProductsBuyClientDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientsAgregator_DAL.Models
+{
+    public class ProductsBuyClientDTO
+    {
+        public int Id { get; set; }
+        public string Articul { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int MeasureId { get; set; }
+    }
+}
