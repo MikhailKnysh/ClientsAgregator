d7c50068408e8763343596785334700dc348c8dc
Mikhail <mishadnepr347@gmail.com>
2021-06-03 13:21:35 +0300

    Added OrdersHelper class and OrdersModel

---
+++ ClientsAgregator_BLL/CustomModels/OrdersModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientsAgregator_BLL.CustomModels
+{
+    public class OrdersModel
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int StatusesId { get; set; }
+        public string SellerComment { get; set; }
+        public string OrderDate { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
