0d54745fbadd2d0b7b7ba3326cc2ed778334c867
RDesti <tatotschka17@rambler.ru>
2021-06-11 01:47:19 +0300

    Added validation for AddClientPage

--- ClientsAgregator/Pages/AddClientPage.xaml.cs
+++ ClientsAgregator/Pages/AddClientPage.xaml.cs
@@ -11,6 +11,7 @@ namespace ClientsAgregator.Pages
     public partial class AddClientPage : Page
     {
         private Controller _controller;
+        private List<BulkStatusModel> _bulkStatusModel = new List<BulkStatusModel>();
 
         public AddClientPage()
         {
@@ -24,40 +25,56 @@ namespace ClientsAgregator.Pages
 
         private void buttonSave_Click(object sender, RoutedEventArgs e)
         {
+            int bulkStatusIndex = ComboBoxBulkStatus.SelectedIndex;
             string lastName = TextBoxLastName.Text.Trim();
             string firstName = TextBoxFirstName.Text.Trim();
             string middleName = TextBoxMiddleName.Text.Trim();
-
+            string commentAboutСlient = TextBoxCommentAboutClient.Text.Trim();
             string email = TextBoxEmail.Text.Trim();
             string phone = TextBoxPhone.Text.Trim();
 
             bool isValidEmail = ValidationData.IsValidEmail(email);
             bool isValidPhone = ValidationData.IsValidPhone(phone);
-            bool isValidStringlastName = ValidationData.IsValidStringLenght(lastName, validCharQuantity: 255);
-            bool isValidStringfirstName = ValidationData.IsValidStringLenght(lastName, validCharQuantity: 255);
-            bool isValidStringmiddleName = ValidationData.IsValidStringLenght(lastName, validCharQuantity: 255);
+            bool isValidStringLastName = ValidationData.IsValidStringLenght(lastName, validCharQuantity: 255);
+            bool isValidStringFirstName = ValidationData.IsValidStringLenght(firstName, validCharQuantity: 255);
+            bool isValidStringMiddleName = ValidationData.IsValidStringLenght(middleName, validCharQuantity: 255);
+            bool isValidStringcomment = ValidationData.IsValidStringLenght(commentAboutСlient, validCharQuantity: 800);
+            bool isValidStringEmail = ValidationData.IsValidStringLenght(commentAboutСlient, validCharQuantity: 50);
+            bool isValidStringPhone = ValidationData.IsValidStringLenght(commentAboutСlient, validCharQuantity: 60);
 
-
-            if (isValidStringlastName == false)
+            if (isValidStringLastName == false)
             {
                 TextBoxLastName.ToolTip = "Это поле введено некорректно";
                 TextBoxLastName.Background = Brushes.Tomato;
             }
-            else if (isValidPhone == false)
+            else if (isValidStringFirstName == false)
+            {
+                TextBoxFirstName.ToolTip = "Это поле введено некорректно";
+                TextBoxFirstName.Background = Brushes.Tomato;
+            }
+            else if (isValidStringMiddleName == false)
+            {
+                TextBoxMiddleName.ToolTip = "Это поле введено некорректно";
+                TextBoxMiddleName.Background = Brushes.Tomato;
+            }
+            else if (isValidStringcomment == false)
+            {
+                TextBoxCommentAboutClient.ToolTip = "Это поле введено некорректно";
+                TextBoxCommentAboutClient.Background = Brushes.Tomato;
+            }
+            else if (isValidPhone == false || isValidStringPhone == false)
             {
                 TextBoxPhone.ToolTip = "Это поле введено некорректно";
                 TextBoxPhone.Background = Brushes.Tomato;
             }
-            else if (isValidEmail == false)
+            else if (isValidEmail == false || isValidStringEmail == false)
             {
+                TextBoxEmail.ToolTip = "Это поле введено некорректно";
                 TextBoxPhone.Background = Brushes.Transparent;
                 TextBoxEmail.Background = Brushes.Tomato;
             }
             else
             {
-                TextBoxLastName.Background = Brushes.Transparent;
-                TextBoxEmail.Background = Brushes.Transparent;
-
                 AddClientModel addClientModel = new AddClientModel()
                 {
                     LastName = lastName,
@@ -65,7 +82,7 @@ namespace ClientsAgregator.Pages
                     MiddleName = middleName,
                     Phone = phone,
                     Email = email,
-                    BulkStatusId = ComboBoxBulkStatus.SelectedIndex,
+                    BulkStatusId = _bulkStatusModel[bulkStatusIndex].Id,
                     Male = ComboBoxMale.Text,
                     СommentAboutСlient = TextBoxCommentAboutClient.Text
                 };
@@ -74,23 +91,31 @@ namespace ClientsAgregator.Pages
 
                 MessageBox.Show("Клиент добавлен");
 
-                TextBoxLastName.Clear();
-                TextBoxFirstName.Clear();
-                TextBoxMiddleName.Clear();
-                TextBoxPhone.Clear();
-                TextBoxEmail.Clear();
-                TextBoxCommentAboutClient.Clear();
-                ComboBoxBulkStatus.Text = string.Empty;
-                ComboBoxMale.Text = string.Empty;
+                foreach (UIElement element in AddClientGrid.Children)
+                {
+                    if (element is TextBox)
+                    {
+                        TextBox textBox = (TextBox)element;
+                        textBox.Background = Brushes.Transparent;
+                        textBox.Clear();
+                    }
+
+                    if (element is ComboBox)
+                    {
+                        ComboBox comboBox = (ComboBox)element;
+                        comboBox.Background = Brushes.Transparent;
+                        comboBox.Text = string.Empty;
+                    }
+                }
             }
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             _controller = new Controller();
-            List<BulkStatusModel> bulkStatusModel = _controller.GetBulkStatusesModels();
+            _bulkStatusModel = _controller.GetBulkStatusesModels();
 
-            foreach (var item in bulkStatusModel)
+            foreach (var item in _bulkStatusModel)
             {
                 ComboBoxBulkStatus.Items.Add(item.Title);
             }
