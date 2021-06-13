d17641eff6f8aae7a198e3a413ce1faf7faebf9e
Sergei <nickincorname@gmail.com>
2021-06-14 01:28:37 +0300

    Added tests and problem with testing ProductBuy

---
+++ ClientsAgregator_BLL.Test/TestClases/ProductsBuyClientAndFeedback.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClientsAgregator_BLL.CustomModels;
+using ClientsAgregator_DAL.Interface;
+using Moq;
+using NUnit.Framework;
+
+namespace ClientsAgregator_BLL.Test.TestClases
+{
+    class ProductsBuyClientAndFeedback
+    {
+        private Mock<List<FeedbackModel>> _mock;
+        
+        [SetUp]
+
+        public void Setup()
+        {
+            _mock = new Mock<List<FeedbackModel>>();
+
+        }
+
+        public void qqqqq()
+        {
+            _mock.Setup()
+        }
+
+    }
+}
